c35d263c73f2e035cbbd50bb5a25010ebfbfef88
KovalevAnton <a.kovalev@formind.pro>
2024-05-26 16:44:15 -0300

    add reservation feature

--- app/myproperties/page.tsx
+++ app/myproperties/page.tsx
@@ -1,8 +1,8 @@
-// import { getUserId } from "../lib/actions";
+import { getUserId } from "../lib/actions";
 import PropertyList from '../components/properties/PropertyList';
 
 const MyPropertiesPage = async () => {
-  // const userId = await getUserId();
+  const userId = await getUserId();
 
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6">
@@ -10,7 +10,7 @@ const MyPropertiesPage = async () => {
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <PropertyList
-        // landlord_id={userId}
+        landlord_id={userId}
         />
       </div>
     </main>

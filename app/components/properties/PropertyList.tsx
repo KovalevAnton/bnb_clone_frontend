932f80acfadf21e81ceef6faeb34149679b5b1cf
KovalevAnton <a.kovalev@formind.pro>
2024-05-26 17:52:17 -0300

    add favorites

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -14,22 +14,52 @@ export type PropertyType = {
 
 interface PropertyListProps {
   landlord_id?: string | null;
+  favorites?: boolean | null;
 }
 
-const PropertyList: React.FC<PropertyListProps> = ({ landlord_id }) => {
+const PropertyList: React.FC<PropertyListProps> = ({
+  landlord_id,
+  favorites,
+}) => {
   const [properties, setProperties] = useState<PropertyType[]>([]);
+
+  const markFavorite = (id: string, is_favorite: boolean) => {
+    const tmpProperties = properties.map((property: PropertyType) => {
+      if (property.id == id) {
+        property.is_favorite = is_favorite;
+
+        if (is_favorite) {
+          console.log('added to list of favorited propreties');
+        } else {
+          console.log('removed from list');
+        }
+      }
+
+      return property;
+    });
+
+    setProperties(tmpProperties);
+  };
 
   const getProperties = async () => {
     let url = '/api/properties/';
 
     if (landlord_id) {
       url += `?landlord_id=${landlord_id}`;
+    } else if (favorites) {
+      url += '?is_favorites=true';
     }
 
     const tmpProperties = await apiService.get(url);
 
     setProperties(
       tmpProperties.data.map((property: PropertyType) => {
+        if (tmpProperties.favorites.includes(property.id)) {
+          property.is_favorite = true;
+        } else {
+          property.is_favorite = false;
+        }
+
         return property;
       })
     );
@@ -42,7 +72,15 @@ const PropertyList: React.FC<PropertyListProps> = ({ landlord_id }) => {
   return (
     <>
       {properties.map((property) => {
-        return <PropertyListItem key={property.id} property={property} />;
+        return (
+          <PropertyListItem
+            key={property.id}
+            property={property}
+            markFavorite={(is_favorite: any) =>
+              markFavorite(property.id, is_favorite)
+            }
+          />
+        );
       })}
     </>
   );
